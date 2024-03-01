fb85f5a00f927ce63cd66fe912b9b7ddea0cb27a
Joel Sanchez <joel.sanchez.ruas@udc.es>
2024-03-01 18:11:39 +0100

    Main activity

--- app/src/main/java/com/example/amp_jam/MainActivity.kt
+++ app/src/main/java/com/example/amp_jam/MainActivity.kt
@@ -15,14 +15,7 @@ import com.example.amp_jam.ui.theme.AMP_JAMTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AMP_JAMTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.main_activity)
     }
 }
 
