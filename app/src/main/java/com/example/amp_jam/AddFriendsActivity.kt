407466a74f281202622b28b23fea85745639f4b6
María Gandoy <maria.gandoy@udc.es>
2024-03-19 11:53:22 +0100

    Estructura de archivos

---
+++ app/src/main/java/com/example/amp_jam/AddFriendsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.amp_jam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class AddFriendsActivity : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_friends)
+    }
+}
---
+++ app/src/main/java/com/example/amp_jam/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.amp_jam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class ProfileActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile)
+    }
+}
