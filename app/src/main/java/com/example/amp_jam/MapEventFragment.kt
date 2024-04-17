98a8f65703795225badb09c187427609d8dcbe38
angela martin <angela.martin@udc.es>
2024-04-18 00:54:34 +0200

    Modifications to posts creation

--- app/src/main/java/com/example/amp_jam/MapEventFragment.kt
+++ app/src/main/java/com/example/amp_jam/MapEventFragment.kt
@@ -13,6 +13,9 @@ import android.widget.ImageButton
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.fragment.app.DialogFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import java.util.Calendar
 
 class MapEventFragment: DialogFragment() {
@@ -23,11 +26,19 @@ class MapEventFragment: DialogFragment() {
 
     var listener: MapEventDialogListener? = null
 
+    private lateinit var auth: FirebaseAuth
+
+    private var currentUser: FirebaseUser? = null
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
             val inflater = requireActivity().layoutInflater;
             val view = inflater.inflate(R.layout.map_event, null)
+
+            // Retrieve current user
+            auth = FirebaseAuth.getInstance()
+            currentUser = auth.currentUser
 
             setUpExitButton(view)
             setUpNameEventListener(view)
@@ -116,13 +127,22 @@ class MapEventFragment: DialogFragment() {
         val eventName = view.findViewById<EditText>(R.id.eventName).text.toString()
         val eventDate = view.findViewById<EditText>(R.id.eventDate).text.toString()
         val eventType = "EVENT" // TODO: add SONG and PHOTO
+        val userEmail = currentUser?.email ?: ""
 
-        return Post(eventName, eventDate, eventType, "", "")
+        return Post(eventName, eventDate, eventType, userEmail, null, null)
     }
 
     private fun submitEvent(data: Post) {
+        persistEvent(data)
         listener?.onEventSubmitted(data)
         dismiss()
+    }
+
+    private fun persistEvent(data: Post) {
+        val database = FirebaseFirestore.getInstance()
+        val usuariosCollection = database.collection("usuarios")
+
+        // TODO: persist event
     }
 
 }
--- app/src/main/java/com/example/amp_jam/Post.kt
+++ app/src/main/java/com/example/amp_jam/Post.kt
@@ -1,8 +1,9 @@
 package com.example.amp_jam
 
-data class Post ( var title: String,
-                  var date: String,
-                  var type: String,
-                  var user: String,
-                  var photo: String ) {}
+data class Post(var title: String?,
+                var date:  String?,
+                var type: String,
+                var user: String,
+                var photo: String?,
+                var song: String? ) {}
 
