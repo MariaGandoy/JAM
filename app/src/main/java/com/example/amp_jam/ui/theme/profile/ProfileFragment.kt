d20b6ef2b8547c9ff6812647dc9883031d56f44c
Juanolas1 <juan.tfreire@udc.es>
2024-05-19 13:05:39 +0200

    BOton para añadir grupos del perfil.

--- app/src/main/java/com/example/amp_jam/ui/theme/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/amp_jam/ui/theme/profile/ProfileFragment.kt
@@ -9,24 +9,22 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.Switch
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.amp_jam.GroupCreateDialog
 import com.example.amp_jam.ListOfFriendsActivity
 import com.example.amp_jam.Post
 import com.example.amp_jam.R
 import com.example.amp_jam.RecyclerAdapter
 import com.example.amp_jam.SettingsActivity
-import com.example.amp_jam.User
 import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -42,6 +40,8 @@ private const val ARG_PARAM2 = "param2"
 class ProfileFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
+    private lateinit var locationSwitch: Switch
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
     val mAdapter: RecyclerAdapter = RecyclerAdapter()
     lateinit var mRecyclerView: RecyclerView
@@ -59,7 +59,15 @@ class ProfileFragment : Fragment() {
 
         setUpSettings(view)
         loadUserProfileData(view)
+        setupAddGroupsButton(view)
 
+        val settingsButton = view.findViewById<ImageButton>(R.id.configurations)
+        settingsButton.setOnClickListener {
+            val intent = Intent(activity, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        Log.d("Debugeandoklk", "llego Profile")
         return view
     }
 
@@ -70,6 +78,9 @@ class ProfileFragment : Fragment() {
             startActivity(intent)
         }
     }
+
+
+
 
     private fun loadUserProfileData(view: View) {
         val currentUser = auth.currentUser
@@ -84,9 +95,9 @@ class ProfileFragment : Fragment() {
             currentUser.photoUrl?.let {
                 Glide.with(requireContext())
                     .load(it)
-                    .placeholder(R.drawable.sample_user)
+                    .placeholder(R.drawable.logo)
                     .into(profileImageView)
-            } ?: profileImageView.setImageResource(R.drawable.sample_user)
+            } ?: profileImageView.setImageResource(R.drawable.logo)
 
             // Load user name
             firestore.collection("usuarios").document(currentUser.uid)
@@ -101,10 +112,7 @@ class ProfileFragment : Fragment() {
                 }
 
             // Load posts
-            lifecycleScope.launch {
-                val user = getUser(currentUser.uid, firestore)
-                setUpRecyclerView(view, user)
-            }
+            setUpRecyclerView(view)
 
             firestore.collection("usuarios").document(currentUser.uid).collection("friends")
                 .get()
@@ -129,7 +137,7 @@ class ProfileFragment : Fragment() {
         }
     }
 
-    private fun setUpRecyclerView(view: View, user: User?) {
+    private fun setUpRecyclerView(view: View) {
         mRecyclerView = view.findViewById<RecyclerView>(R.id.postsList)
         mRecyclerView.setHasFixedSize(true)
         mRecyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -137,15 +145,14 @@ class ProfileFragment : Fragment() {
         progressBar.visibility = View.VISIBLE
         customMessage.visibility = View.GONE
 
-        // Fetch posts asynchronously and set up the adapter when posts are available
-        getPosts(user) { posts ->
+        getPosts { posts ->
             mAdapter.RecyclerAdapter(posts, requireContext(), findNavController())
             mRecyclerView.adapter = mAdapter
             progressBar.visibility = View.GONE
         }
     }
 
-    private fun getPosts(user: User?, callback: (MutableList<Post>) -> Unit) {
+    private fun getPosts(callback: (MutableList<Post>) -> Unit) {
         var posts:MutableList<Post> = ArrayList()
 
         val auth = FirebaseAuth.getInstance()
@@ -159,12 +166,11 @@ class ProfileFragment : Fragment() {
                         for (postDocument in documents) {
                             val postData = postDocument.data
 
-                            // Get post location data
                             val lugarPost = postData["lugar"] as HashMap<*,*>
                             val latitude = lugarPost["latitude"] as Double
                             val longitude = lugarPost["longitude"] as Double
 
-                            posts.add(Post(postData["titulo"], postData["fecha"], postData["tipo"], user, null, postData["song"], LatLng(latitude, longitude)))
+                            posts.add(Post(postData["titulo"], postData["fecha"], postData["tipo"], null, null, null, LatLng(latitude, longitude)))
                         }
                     } else {
                         customMessage.visibility = View.VISIBLE
@@ -181,17 +187,12 @@ class ProfileFragment : Fragment() {
                 }
         }
     }
-
-    private suspend fun getUser(userId: String, database: FirebaseFirestore): User? {
-        return try {
-            val document = database.collection("usuarios").document(userId).get().await()
-            val name = document.getString("name")
-            val photo = document.getString("photo")
-            val email = document.getString("email")
-
-            return User(name, userId, email, photo)
-        } catch (e: Exception) {
-            null
+    private fun setupAddGroupsButton(view: View) {
+        val addGroupsButton = view.findViewById<ImageButton>(R.id.addGroups)
+        addGroupsButton.setOnClickListener {
+            val groupCreateDialog = GroupCreateDialog()
+            groupCreateDialog.show(requireFragmentManager(), "GroupCreateDialog")
         }
     }
+
 }
